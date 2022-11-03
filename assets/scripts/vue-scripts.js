b9410106fac1e7116763f22bcfa118ab4e1c9edb
gil-cunha <gilcunha@outlook.pt>
2022-11-03 13:14:27 +0000

    add vue-scripts

--- assets/scripts/vue-scripts.js
+++ assets/scripts/vue-scripts.js
@@ -1,10 +1,9 @@
-
-        const myVue = new Vue(
-            {
-                el: '#app',
-                data: { 
-                    message: 'Olá, Vue.js!',
-                    summary: 'A minha primeira aplicação Vue.'
-                }
-            }
-        )    
+const myVue = new Vue(
+    {
+        el: '#app',
+        data: { 
+            message: 'Olá, Vue.js!',
+            summary: 'A minha primeira aplicação Vue.'
+        }
+    }
+)    
