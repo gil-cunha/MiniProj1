39860365eaa6301a1657de9521615325c494462a
gil-cunha <gilcunha@outlook.pt>
2022-11-03 13:14:02 +0000

    new site architecture

---
+++ assets/scripts/vue-scripts.js
@@ -0,0 +1,10 @@
+
+        const myVue = new Vue(
+            {
+                el: '#app',
+                data: { 
+                    message: 'Olá, Vue.js!',
+                    summary: 'A minha primeira aplicação Vue.'
+                }
+            }
+        )    
