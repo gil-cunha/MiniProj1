4e4753248b7b6d203e875e7f069ea7be9859163a
gil-cunha <gilcunha@outlook.pt>
2022-11-04 11:52:11 +0000

    scripts

---
+++ assets/scripts/callbacks.js
@@ -0,0 +1,15 @@
+  window.onload = function(){
+    function whoAmI(){
+        console.log("i did something");
+    }
+    
+    function whatIDo(){
+        console.log("i did something again");
+    }
+    
+    function hobbies(){
+        console.log("i did something again");
+    }  
+    
+    setYear()
+}
--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,15 +1,10 @@
-window.onload = function(){
-
-    setYear()
-
     function getTheYear(){
         const d = new Date()
         let year = d.getFullYear()
         return year
     }
-
+    
     function setYear(){
         const el = document.getElementById("year")
         el.innerHTML = getTheYear() 
-    }
-}
+    }            
